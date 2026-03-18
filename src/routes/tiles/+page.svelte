93bfa4c4a9684decc60503fb66adb378f6eb7c2f
robert.tootill <robert.tootill98@gmail.com>
2026-03-18 19:05:16 +0000

    file input

--- src/routes/tiles/+page.svelte
+++ src/routes/tiles/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+    import { Input } from "$lib/components/ui/input/index.js";
 
+    //#region file input
+    let files: FileList | undefined = $state(undefined);
+    //#endregion file input
 </script>
 
-<!-- title -->
-Tiles
+<!-- file input -->
+<div class="flex gap-2 items-center">
+    <!-- label -->
+    <span class="w-fit">Tiles file upload:</span>
+
+    <!-- input -->
+    <Input class="w-fit" type="file" bind:files/>
+</div>
