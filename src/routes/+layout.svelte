79a199629a78f704b0ba7622e21c4916b1c451ac
robert.tootill <robert.tootill98@gmail.com>
2026-03-18 19:14:13 +0000

    basic favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import greatBall from '$lib/assets/greatBall-removebg-preview.png';
 	import ToggleTheme from "$lib/components/ToggleTheme.svelte";
-
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={greatBall} /></svelte:head>
 
-<div class="h-screen w-screen flex gap-4 flex-col bg-background text-white">
+<div class="h-screen w-screen flex gap-4 flex-col bg-background">
 	<header class="flex justify-end w-screen p-2">
 		<ToggleTheme/>
 	</header>
