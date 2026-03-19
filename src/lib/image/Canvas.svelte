664a848d7e19bc490d1bb52ef2a9e2983002958b
robert.tootill <robert.tootill98@gmail.com>
2026-03-19 21:31:08 +0000

    add disabled pixel width scaling

--- src/lib/image/Canvas.svelte
+++ src/lib/image/Canvas.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Colour } from "./ImageManager";
     import * as Card from "$lib/components/ui/card/index.js";
+    import Input from "$lib/components/ui/input/input.svelte";
 
     let {
         pixels,
@@ -12,18 +13,20 @@
 
     let canvas: HTMLCanvasElement;
 
-    let height: number = $derived(pixels.length);
     let width: number = $derived(pixels[0].length);
+    let height: number = $derived(pixels.length);
+
+    const pixelWidth: number = 4;
 
     $effect(() => {
-        if(pixels) {
+        if(pixels && scaleFactor) {
             setCanvas();
         }
     });
 
     function setCanvas(): void {
         const pixels_render = new Uint8ClampedArray(width * height * pixelWidth);
-        
+
         let pixel_render_index: number = 0;
 
         for(let y = 0; y < pixels.length; y++) {
@@ -49,7 +52,7 @@
                             filterBy.blue == colour.blue
                         )
                     ) {
-                        const gray: number = (colour.red + colour.green + colour.blue) / 3;
+                        const gray: number = 0.3 * colour.red + 0.59 * colour.green + 0.11 * colour.blue;
 
                         pixels_render[pixel_render_index + i] =
                             gray * (1 - muteFactor) + colourValue * muteFactor;
@@ -62,11 +65,20 @@
             }
         }
 
-        const imageData = new ImageData(pixels_render, width, height);
-        canvas.getContext('2d')!.putImageData(imageData, 0, 0);
+        // scale
+        canvas.width = Math.floor(width * scaleFactor);
+        canvas.height = Math.floor(height * scaleFactor);
+
+        const ctx = canvas.getContext('2d')!;
+        
+        // clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // load image data
+        ctx.putImageData(new ImageData(pixels_render, width, height), 0, 0);
     }
 
-    let pixelWidth: number = $state(4);
+    let scaleFactor: number = $state(1);
     let muteFactor: number = $state(0.1);
 </script>
 
@@ -76,10 +88,23 @@
     </Card.Header>
 
     <Card.Content>
-        <canvas bind:this={canvas} {height} {width} style="image-rendering: pixelated;"></canvas>
+        <canvas bind:this={canvas} style="image-rendering: pixelated;"></canvas>
+        <!-- <canvas bind:this={canvas} {height} {width} style="image-rendering: pixelated;"></canvas> -->
     </Card.Content>
 
-    <Card.Footer>
+    <Card.Footer class="flex flex-col gap-2 items-start">
         <span>Dimensions: {pixels!.length} x {pixels![0].length}</span>
+
+        <!-- #region controls -->
+        
+        <!-- pixel width -->
+        <!--
+        <div class="flex items-center gap-2">
+            <span>Size:</span>
+            <Input type="range" bind:value={scaleFactor} min={1} max={10} step={0.1}/>
+        </div>
+        -->
+
+        <!-- #endregion controls -->
     </Card.Footer>
 </Card.Root>
