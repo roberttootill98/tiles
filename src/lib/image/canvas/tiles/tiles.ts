216833ebd87260d75a92339811dbfbfcaa909aa5
robert.tootill <robert.tootill98@gmail.com>
2026-04-06 11:01:04 +0100

    respect tile flipping

--- src/lib/image/canvas/tiles/tiles.ts
+++ src/lib/image/canvas/tiles/tiles.ts
@@ -3,6 +3,27 @@ import { compareColours, type Colour } from '$lib/colour';
 export function compareTiles(tile1: Colour[][], tile2: Colour[][]): boolean {
 	if (tile1.length != tile2.length) return false;
 
+	// generate flipped versions of tile 1
+	const tile1_flippedX: Colour[][] = flipInX(tile1);
+	const tile1_flippedY: Colour[][] = flipInY(tile1);
+	const tile1_flippedXY: Colour[][] = flipInY(flipInX(tile1));
+
+	// regular
+	const compare_regular = compare(tile1, tile2);
+
+	// flip x only
+	const compare_flippedX = compare(tile1_flippedX, tile2);
+
+	// flip y only
+	const compare_flippedY = compare(tile1_flippedY, tile2);
+
+	// flip x and y
+	const compare_flippedXY = compare(tile1_flippedXY, tile2);
+
+	return compare_regular || compare_flippedX || compare_flippedY || compare_flippedXY;
+}
+
+function compare(tile1: Colour[][], tile2: Colour[][]): boolean {
 	for (let row = 0; row < tile1.length; row++) {
 		if (tile1[row].length != tile2[row].length) return false;
 
@@ -14,4 +35,30 @@ export function compareTiles(tile1: Colour[][], tile2: Colour[][]): boolean {
 	}
 
 	return true;
+}
+
+function flipInX(tile: Colour[][]): Colour[][] {
+	const tile_new: Colour[][] = [];
+
+	for (let row = 0; row < tile.length; row++) {
+		const row_new: Colour[] = [];
+
+		for (let column = tile[row].length - 1; column > 0; column--) {
+			row_new.push(tile[row][column]);
+		}
+
+		tile_new.push(row_new);
+	}
+
+	return tile_new;
+}
+
+function flipInY(tile: Colour[][]): Colour[][] {
+	const tile_new: Colour[][] = [];
+
+	for (let row = tile.length - 1; row > 0; row--) {
+		tile_new.push(tile[row]);
+	}
+
+	return tile_new;
 }
